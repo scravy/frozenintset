a877f57158197fe1a5d7c63a61d0dc92921b91df
Julian Bertram <tirednesscankill@warhog.net>
2025-05-30 06:01:22 +0200

    Fix noxfile

--- noxfile.py
+++ noxfile.py
@@ -2,5 +2,7 @@ import nox
 
 @nox.session(python=["3.11", "3.12", "3.13"])
 def tests(session):
-    session.run("uv", "sync", "--active", "-q", external=True)
+    session.run("uv", "pip", "install", ".", external=True)
+    session.run("uv", "pip", "install", "pytest", external=True)
+    session.run("python", "--version")
     session.run("pytest")
